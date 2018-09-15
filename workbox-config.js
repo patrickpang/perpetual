b66f82503d7c9d0313a6c038b2ac62d418ae6cb4
Patrick Pang <imptrik@gmail.com>
2018-09-15 09:11:38 +0800

    cache suitable iconfont

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,11 @@
 module.exports = {
   globDirectory: 'build/',
   globPatterns: ['**/*.{png,html,js,css}', 'manifest.json'],
+  runtimeCaching: [
+    {
+      urlPattern: /\.(ttf|eot|woff|woff2)$/,
+      handler: 'cacheFirst',
+    },
+  ],
   swDest: 'build/service-worker.js',
 }
